ca9f6aa47902a9c518876541d0c123778024cade
louwjlab <louwjlabuschagne@gmail.com>
2020-05-30 15:01:16 +0200

    Dummy tests

---
+++ product-manager.test.js
@@ -0,0 +1,5 @@
+const pm = require('./product-manager')
+
+test("Dummy Test", () => {
+    expect(3).toBe(3)
+})
---
+++ products-client.test.js
@@ -0,0 +1,5 @@
+const pc = require('./product-client')
+
+test("Dummy Test", () => {
+    expect(3).toBe(3)
+})
